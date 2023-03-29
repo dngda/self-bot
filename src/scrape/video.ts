d39c4cd1026dd59b1b75c3830aea5d5cab99c308
dngda <opowaekeno123@gmail.com>
2023-03-30 05:29:50 +0700

    🔗 change api

--- src/scrape/video.ts
+++ src/scrape/video.ts
@@ -1,6 +1,6 @@
 // support url youtube, facebook, instagram, tiktok, twitter
 import axios from 'axios'
-const API_URL = 'https://sh.xznsenpai.xyz/download?url='
+const API_URL = 'https://sh.xznsenpai.xyz/api/download?url='
 
 export const tiktokScraper = async (url: string) => {
   const { data } = await axios.get(`${API_URL}${url}`)
