69c02cdeec482e52b42f636789c978e08412c528
Danang Dwiyoga A <opowaekeno123@gmail.com>
2024-02-18 21:11:24 +0700

    🐞 fix(handler): msgTimestamp nully

--- src/handler.ts
+++ src/handler.ts
@@ -198,7 +198,7 @@ const storeMessageData = (msg: WAMessage) => {
   if (!key) return null
   if (msg.message?.protocolMessage) return null
 
-  storeMessage(key.id!, msg.messageTimestamp, msg.message!)
+  storeMessage(key.id!, msg.messageTimestamp!, msg.message!)
   return true
 }
 
