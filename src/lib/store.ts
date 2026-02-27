d37304bf7da0ffa41257eeb65c0874b50656d69f
Danang Dwiyoga A <opowaekeno123@gmail.com>
2026-02-27 15:12:47 +0700

    refactor: remove unused Long type import from store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,6 @@
 import { proto, WAMessage } from 'baileys'
 import fs from 'fs/promises'
 import path from 'path'
-import type Long from 'long'
 
 interface StoredMessage {
     timestamp: number | Long
